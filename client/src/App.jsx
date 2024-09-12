81970163f84a8c13113622357a80937a6f3448a8
koliandrik <koliandrik@gmail.com>
2024-09-11 19:51:26 -0700

    fixed some dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import OrganPage from './pages/OrganPage';
-import AnimalPage from './pages/AnimalPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
+import HomePage from './components/HomePage';
+import OrganPage from './components/OrganPage';
+import AnimalPage from './components/AnimalPage';
+import CartPage from './components/CartPage';
+import CheckoutPage from './components/CheckoutPage';
 import NavBar from './components/NavBar';
 
 const App = () => {
