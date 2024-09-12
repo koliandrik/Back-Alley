a953d5806c747a0a81a094ded6e41880b09e77cc
koliandrik <koliandrik@gmail.com>
2024-09-11 20:39:45 -0700

    mild reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import OrganPage from './components/OrganPage';
-import AnimalPage from './components/AnimalPage';
-import CartPage from './components/CartPage';
-import CheckoutPage from './components/CheckoutPage';
+// import { Router, Route, Routes } from 'react-router-dom';
+// import HomePage from './components/HomePage';
+// import OrganPage from './components/OrganPage';
+// import AnimalPage from './components/AnimalPage';
+// import CartPage from './components/CartPage';
+// import CheckoutPage from './components/CheckoutPage';
 import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/organs" component={OrganPage} />
-        <Route path="/animals" component={AnimalPage} />
-        <Route path="/cart" component={CartPage} />
-        <Route path="/checkout" component={CheckoutPage} />
-      </Switch>
+      {/* <Router>
+        <Routes>
+          <Route path="/" component={HomePage} />
+          <Route path="/organs" component={OrganPage} />
+          <Route path="/animals" component={AnimalPage} />
+          <Route path="/cart" component={CartPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+        </Routes>
+      </Router> */}
     </div>
   );
 };
